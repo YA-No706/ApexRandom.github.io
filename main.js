c7e6ddd12f938c0a31bee4570df0b245a14d60fd
YA <work.yuki123@gmail.com>
2023-07-20 21:07:38 +0900

    武器タイプのフィルターを追加

--- main.js
+++ main.js
@@ -1,34 +1,68 @@
-var WeaponList = [
-    'G7スカウト',
-    'R301',
-    'ヘムロック',
-    'フラットライン',
-    'ハボック',
-    'プラウラー',
-    'R99',
-    'オルタネーター',
-    'ボルト',
-    'スピットファイア',
-    'ディボーション',
-    'Lスター',
-    'クレーバー',
-    'トリプルテイク',
-    'ロングボウ',
-    'チャージライフル',
-    'センチネル',
-    'モザンビーク',
-    'マスティフ',
-    'ピースキーパー',
-    'EVA8',
-    'ウィングマン',
-    'RE45',
-    'P2020',
-    '30-30リピーター',
-    'ボセック',
-    'ランページLMG',
-    'C.A.R. SMG',
-    'ネメシス'
+function Weapon(_name, _type){
+    this.name = _name;
+    this.type = _type;
+}
+
+/*
+SP : ケアパケ武器
+AR : アサルトライフル
+SMG : サブマシンガン
+LMG : ライトマシンガン
+SR : スナイパーライフル
+SG : ショットガン
+MR : マークスマンライフル
+HG :　ピストル
+*/
+
+var WeaponList_SP = [
+    new Weapon('ヘムロック',"SP"),
+    new Weapon('Lスター',"SP"),
+    new Weapon('クレーバー',"SP"),
+    new Weapon('ボセック',"SP"),
 ]
+
+var WeaponList_AR = [
+    new Weapon('R301',"AR"),
+    new Weapon('フラットライン',"AR"),
+    new Weapon('ハボック',"AR"),
+    new Weapon('ネメシス',"AR")
+
+]
+var WeaponList_SMG = [
+    new Weapon('プラウラー',"SMG"),
+    new Weapon('R99',"SMG"),
+    new Weapon('オルタネーター',"SMG"),
+    new Weapon('ボルト',"SMG"),
+    new Weapon('C.A.R. SMG',"SMG")
+]
+var WeaponList_LMG = [
+    new Weapon('スピットファイア',"LMG"),
+    new Weapon('ディボーション',"LMG"),
+    new Weapon('ランページLMG',"LMG")
+]
+var WeaponList_SR = [
+    new Weapon('ロングボウ',"SR"),
+    new Weapon('チャージライフル',"SR"),
+    new Weapon('センチネル',"SR")
+]
+var WeaponList_SG = [
+    new Weapon('モザンビーク',"SG"),
+    new Weapon('マスティフ',"SG"),
+    new Weapon('ピースキーパー',"SG"),
+    new Weapon('EVA8',"SG")
+]
+var WeaponList_MR = [
+    new Weapon('G7スカウト',"MR"),
+    new Weapon('トリプルテイク',"MR"),
+    new Weapon('30-30リピーター',"MR")
+
+]
+var WeaponList_HG = [
+    new Weapon('RE45',"HG"),
+    new Weapon('P2020',"HG"),
+    new Weapon('ウィングマン',"HG")
+]
+
 var CharacterList = [
     'ブラッドハウンド',
     'ジブラルタル',
@@ -56,16 +90,69 @@ var CharacterList = [
     'バリスティック'
 ]
 
-
 function Execute(){
     var weaponText = document.getElementById("weapon");
     var characterText = document.getElementById("character");
 
-    var weapon_0 = WeaponList[Math.floor(Math.random() * WeaponList.length)]
-    var weapon_1 = WeaponList[Math.floor(Math.random() * WeaponList.length)]
-    var character = CharacterList[Math.floor(Math.random() * CharacterList.length)]
+    var WeaponList = [];
 
-    weaponText.innerHTML = weapon_0 + " / " + weapon_1;
-    characterText.innerHTML = character;
+    //-----------------
+    var checkbox = document.getElementById("checkbox_SP");
+    if (checkbox.checked)
+    {
+        WeaponList = WeaponList.concat(WeaponList_SP);
+    }
+    checkbox = document.getElementById("checkbox_AR");
+    if (checkbox.checked)
+    {
+        WeaponList = WeaponList.concat(WeaponList_AR);
+    }
+    checkbox = document.getElementById("checkbox_SMG");
+    if (checkbox.checked)
+    {
+        WeaponList = WeaponList.concat(WeaponList_SMG);
+    }
+    checkbox = document.getElementById("checkbox_LMG");
+    if (checkbox.checked)
+    {
+        WeaponList = WeaponList.concat(WeaponList_LMG);
+    }
+    checkbox = document.getElementById("checkbox_SR");
+    if (checkbox.checked)
+    {
+        WeaponList = WeaponList.concat(WeaponList_SR);
+    }
+    checkbox = document.getElementById("checkbox_SG");
+    if (checkbox.checked)
+    {
+        WeaponList = WeaponList = WeaponList.concat(WeaponList_SG);
+    }
+    checkbox = document.getElementById("checkbox_MR");
+    if (checkbox.checked)
+    {
+        WeaponList = WeaponList.concat(WeaponList_MR);
+    }
+    checkbox = document.getElementById("checkbox_HG");
+    if (checkbox.checked)
+    {
+        WeaponList = WeaponList.concat(WeaponList_HG);
+    }
+    console.log(WeaponList);
+    //-----------------
+
+    if(WeaponList.length == 0)
+    {
+        weaponText.innerHTML = "ERROR / ERROR";
+        characterText.innerHTML = "全部チェック外すな";
+    }
+    else
+    {
+        var weapon_0 = WeaponList[Math.floor(Math.random() * WeaponList.length)].name
+        var weapon_1 = WeaponList[Math.floor(Math.random() * WeaponList.length)].name
+        var character = CharacterList[Math.floor(Math.random() * CharacterList.length)]
+    
+        weaponText.innerHTML = weapon_0 + " / " + weapon_1;
+        characterText.innerHTML = character;
+    }
 
 }
